8d261fe03b15c157d1b716b6d2ea196899ee7c8c
szepesivirag <szgvirag54@gmail.com>
2020-10-11 16:25:19 +0200

    Kezdo file-ok

---
+++ betumeret.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int a=1;
+    do{
+        int meret=0;
+        char ur;
+        printf("Kerem adja meg a betu meretet: ");
+        if ( ( (scanf("%d",&meret))==1) && (meret%2==1) && (meret>0) ){
+            for(int i=0;i<meret;i++){
+                if (meret!=3){
+                    if ( (i==0) | (i==meret/2) | (i==meret-1) ){
+                        for(int j=0;j<(meret);j++){
+                            printf("*");
+                        }
+                    }else {
+                        printf("*");
+                    }
+                }
+                else {
+                    printf("A 3-as szammal nem lehet E karaktert kiírni.");
+                    break;
+                }
+                printf("\n");
+            }
+            a=0;
+        }else{
+            printf("\nValami hiba van a megadott adattal!\n");
+            while(ur=getchar() != '\n'){}
+        }
+
+
+
+
+    } while(a);
+
+    return 0;
+}
